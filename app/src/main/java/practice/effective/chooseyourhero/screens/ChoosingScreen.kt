990cd6f25ed25ec8cc1770dd5b1035335cfee9f1
iana_osipova <ospvn13@gmail.com>
2022-10-20 15:00:22 +0600

    feat: heroes choosing screen

--- app/src/main/java/practice/effective/chooseyourhero/screens/ChoosingScreen.kt
+++ app/src/main/java/practice/effective/chooseyourhero/screens/ChoosingScreen.kt
@@ -1,2 +1,61 @@
 package practice.effective.chooseyourhero.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.chrisbanes.snapper.ExperimentalSnapperApi
+import dev.chrisbanes.snapper.LazyListSnapperLayoutInfo
+import dev.chrisbanes.snapper.rememberLazyListSnapperLayoutInfo
+import dev.chrisbanes.snapper.rememberSnapperFlingBehavior
+import practice.effective.chooseyourhero.R
+import practice.effective.chooseyourhero.ui.components.HeroCard
+import practice.effective.chooseyourhero.viewmodels.HeroesViewModel
+
+@OptIn(ExperimentalSnapperApi::class)
+@Composable
+internal fun ChoosingScreen() {
+    val heroesViewModel = HeroesViewModel()
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(R.drawable.marvel),
+            contentDescription = "",
+            modifier = Modifier
+                .padding(vertical = 5.dp)
+                .size(150.dp),
+        )
+        Text(
+            text = stringResource(id = R.string.choose_your_hero),
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+        )
+
+        val lazyListState = rememberLazyListState()
+        val layoutInfo: LazyListSnapperLayoutInfo = rememberLazyListSnapperLayoutInfo(lazyListState)
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            state = lazyListState,
+            flingBehavior = rememberSnapperFlingBehavior(lazyListState),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            val items = heroesViewModel.getHeroesList().toList()
+            items(items) { item ->
+                HeroCard(item, items.indexOf(item), layoutInfo)
+            }
+        }
+    }
+}
--- app/src/main/java/practice/effective/chooseyourhero/viewmodels/HeroesViewModel.kt
+++ app/src/main/java/practice/effective/chooseyourhero/viewmodels/HeroesViewModel.kt
@@ -1,4 +1,20 @@
 package practice.effective.chooseyourhero.viewmodels
 
+import practice.effective.chooseyourhero.R
+
 class HeroesViewModel {
+    private var heroesList: Map<String, Int> = mapOf(
+        "Apocalypse" to R.drawable.apocalypse,
+        "Captain America" to R.drawable.captainamerica,
+        "Deadpool" to R.drawable.deadpool,
+        "Iron Man" to R.drawable.ironman,
+        "Nebula" to R.drawable.nebula,
+        "Peggy Carter" to R.drawable.peggycarter,
+        "Scarlet Witch" to R.drawable.scarletwitch,
+        "Sif" to R.drawable.sif,
+        "Spiderman" to R.drawable.spiderman,
+        "Storm" to R.drawable.storm,
+    )
+
+    internal fun getHeroesList(): Map<String, Int> = heroesList
 }
