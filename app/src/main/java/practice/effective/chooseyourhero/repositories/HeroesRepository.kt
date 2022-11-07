ca78207f729dc442b23da527b7e1f33769564dd4
iana_osipova <ospvn13@gmail.com>
2022-11-08 01:07:59 +0600

    chore: add forgotten suspend

--- app/src/main/java/practice/effective/chooseyourhero/repositories/HeroesRepository.kt
+++ app/src/main/java/practice/effective/chooseyourhero/repositories/HeroesRepository.kt
@@ -1,19 +1,15 @@
 package practice.effective.chooseyourhero.repositories
 
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import practice.effective.chooseyourhero.network.MarvelApi
 import practice.effective.chooseyourhero.network.dtos.HeroDto
 
-private const val DELAY: Long = 10
-
 class HeroesRepository(private val api: MarvelApi = MarvelApi) {
-    internal fun getHeroesList(): Flow<List<HeroDto>> {
+    internal suspend fun getHeroesList(): Flow<List<HeroDto>> {
         val res: Flow<List<HeroDto>> = flow {
             val response = api.retrofitService.getAllHeroes().data.results
             emit(response)
-            delay(DELAY)
         }
         return res
     }
@@ -22,7 +18,6 @@ class HeroesRepository(private val api: MarvelApi = MarvelApi) {
         val res: Flow<HeroDto> = flow {
             val response = api.retrofitService.getHeroById(id).data.results.single()
             emit(response)
-            delay(DELAY)
         }
         return res
     }
