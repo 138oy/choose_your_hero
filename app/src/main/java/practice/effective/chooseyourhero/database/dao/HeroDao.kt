9724a589a90138dd30f83b5f01faf63d65f4bd1e
iana_osipova <ospvn13@gmail.com>
2022-11-10 00:45:21 +0600

    chore: chore fixes

--- app/src/main/java/practice/effective/chooseyourhero/database/dao/HeroDao.kt
+++ app/src/main/java/practice/effective/chooseyourhero/database/dao/HeroDao.kt
@@ -1,12 +1,13 @@
 package practice.effective.chooseyourhero.database.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import practice.effective.chooseyourhero.models.Hero
 
-
+@Dao
 interface HeroDao {
     @Query("SELECT * FROM heroes")
     fun getHeroesList(): List<Hero>
